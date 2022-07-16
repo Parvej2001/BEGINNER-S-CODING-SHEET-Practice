f4bec9bf0d6b5f45daf659c56e6260641ccdea5c
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-07-16 17:02:54 +0530

    added ascii programme

---
+++ BASICS/ASCII.cpp
@@ -0,0 +1,13 @@
+// 7) Write a Program to Find ASCII Value of a Character
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "Enter a character : ";
+    cin >> c;
+    cout << "ASCII value of " << c << " is : " << (int)c;
+}
+//OUTPUT
+// Enter a character : P
+// ASCII value of P is : 80
