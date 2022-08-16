ab9832f2a41cf8991cd0c56e9fcadec7d2625bf8
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-16 23:44:53 +0530

    added some codes

--- IF ELSE/largestnum.cpp
+++ IF ELSE/largestnum.cpp
@@ -9,11 +9,11 @@ int main()
     cin >> num1 >> num2 >> num3;
     if (num1 > num2 && num1 > num3)
     {
-        cout << num1 << " is greater.";
+        cout << num1 << " is greater."<<num2<<" and "<< num3;
     }
     else if (num2 > num1 && num2 > num3)
     {
-        cout << num2 << " is greater.";
+        cout << num2 << " is greater."<<num1<<" and "<< num3;
     }
     else
     {
